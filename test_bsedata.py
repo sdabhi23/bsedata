02375fab366e2d8e6ea61972fc0279879c3a52f9
Shrey <shrey.dabhi23@gmail.com>
2019-07-16 02:12:33 +0530

    finish setup for basic tests: getQuote, verifyScripCode

--- test_bsedata.py
+++ test_bsedata.py
@@ -1,14 +1,15 @@
 import pytest
 from bsedata.bse import BSE
 
-b = BSE()
+b = BSE(update_codes=True)
 
 @pytest.mark.parametrize("scripCode",['534976','500116','512573'])
 def test_getQuote_valid(scripCode):
     assert len(b.getQuote(scripCode)) == 27
 
 def test_getQuote_invalid():
-    assert len(b.getQuote('513715')) == 27
+    with pytest.raises(AttributeError):
+        assert b.getQuote('513715')
 
 def test_verifyCode_valid():
     assert b.verifyScripCode('534976') == 'V-mart Retail Ltd.'
