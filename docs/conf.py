b8cf4ff930584a0633ff9d946beb99bda526f0fe
Shrey <shrey.dabhi23@gmail.com>
2021-03-07 19:40:57 +0530

    add dark  theme
    inspired by Pillow docs site (https://pillow.readthedocs.io)

--- docs/conf.py
+++ docs/conf.py
@@ -60,3 +60,6 @@ html_static_path = ['_static']
 master_doc = 'index'
 
 extlinks = {'issue': ('https://github.com/sdabhi23/bsedata/issues/%s','issue ')}
+
+def setup(app):
+    app.add_css_file("darkreader-theme.css")
