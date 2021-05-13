b5457f0178cd9c4b4be9e99933f075ec72d49fd9
sdabhi23 <shrey.dabhi23@gmail.com>
2021-05-13 23:48:45 +0530

    bump version to 0.5.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2018, Shrey Dabhi'
 author = 'Shrey Dabhi'
 
 # The full version, including alpha/beta/rc tags
-release = '0.4.0'
+release = '0.5.0'
 
 
 # -- General configuration ---------------------------------------------------
