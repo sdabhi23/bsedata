63113e0e453d928030cb019732803469de6b0dc5
sdabhi23 <shrey.dabhi23@gmail.com>
2022-08-21 11:06:44 +0530

    remove older versions again

--- setup.py
+++ setup.py
@@ -21,9 +21,6 @@ setuptools.setup(
     ],
     classifiers=(
         "Development Status :: 5 - Production/Stable",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
